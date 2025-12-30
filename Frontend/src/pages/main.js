0c57f6537930f73143d5398605269b539163f36c
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2025-12-30 12:54:20 +0530

    adding navbar

--- Frontend/src/pages/main.js
+++ Frontend/src/pages/main.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/navbar";
 
 function HomePage() {
   return (
     <div>
-      {/* <Navbar /> */}
+      <Navbar />
       <Outlet />
     </div>
   );
